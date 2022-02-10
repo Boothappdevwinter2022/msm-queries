8c028495fd71dcc06a146a1739e2c4088143328f
thaong91 <tnguye13@chicagobooth.edu>
2022-02-10 21:38:40 +0000

    built actor pages

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,13 +2,22 @@ class ActorsController < ApplicationController
   def index
     @list_of_actors = Actor.all
     @list_of_movies = Movie.all
-    render({ :template => "actor_templates/index.html.erb"})
+
+    render({ :template => "actor_templates/index.html.erb" })
   end
 
   def actor_individual_page
-    @list_of_actor = Actor.all
+    @list_of_actors = Actor.all
     @actor_id = params.fetch("actor_id")
-    
-    render({ :template => "actor_templates/actor_individual_page.html.erb"})
+    @current_actor = Actor.where({ :id => @actor_id })
+    @list_of_movies = Character.all.where({ :actor_id => @actor_id })
+
+    @this_year = Time.now.year
+    @year_creation = @list_of_actors.where({ :id => @actor_id }).at(0).created_at.year
+    @year_from_creation = @this_year - @year_creation
+    @year_update = @list_of_actors.where({ :id => @actor_id }).at(0).updated_at.year
+    @year_from_update = @this_year - @year_update
+
+    render({ :template => "actor_templates/actor_individual_page.html.erb" })
   end
 end
